6a0751841608750db1a14fdde7b921dcd6ff68c2
takeda <takeda03731@gmail.com>
2026-04-24 21:14:07 +0900

    4月24日

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ with app.app_context():
 # 表示用固定データ
 # -------------------------
 SITE_INFO = {
-    "construction_name": "令和7年度 管路更新（耐震化）事業　土与丸（是石）地区ほか配水管布設替工事掲示板　4月23日現在",
+    "construction_name": "令和7年度 管路更新（耐震化）事業　土与丸（是石）地区ほか配水管布設替工事掲示板　4月24日現在",
     "image_file": "舗装切断.pdf",  # staticフォルダ内のファイル名
     "image_file2": "工事概要.pdf",
     "image_file3": "臨時駐車場.pdf",
